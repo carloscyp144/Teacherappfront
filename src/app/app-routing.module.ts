a3072502d29bf026f451c04e3124c97cd85af9e0
carloscyp144 <carloscyp144@gmail.com>
2022-11-18 22:54:42 +0100

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { CrearCuentaComponent } from './components/crear-cuenta/crear-cuenta.component';
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",pathMatch:"full",redirectTo:"TeacherApp"},
+  {path:"TeacherApp",component:BuscadorComponent},
+  {path:"TeacherApp/iniciar_sesion",component:IniciarSesionComponent},
+  {path:"TeacherApp/crear_cuenta",component:CrearCuentaComponent},
+  {path:"**",redirectTo:"home"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './components/cabecera/cabecera.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { CrearCuentaComponent } from './components/crear-cuenta/crear-cuenta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    BuscadorComponent,
+    IniciarSesionComponent,
+    CrearCuentaComponent
   ],
   imports: [
     BrowserModule,
